e04a4d31ab89ed5685898e8125cf07ff0e1e37a3
SociuLucian <76247157+SociuLucian@users.noreply.github.com>
2022-03-17 17:14:24 +0200

    .

--- Lab4 Compulsory/src/main/java/Intersection.java
+++ Lab4 Compulsory/src/main/java/Intersection.java
@@ -1,6 +1,11 @@
-public class Intersection {
-
+public class Intersection  {
+     private String Name;
     public Intersection(String s) {
-
+      this.Name=s;
     }
+     public String getName(){
+        return Name;
+     }
+
+
 }
---
+++ Lab4 Compulsory/src/main/java/Street.java
@@ -0,0 +1,10 @@
+import java.util.stream.Collector;
+
+public class Street  {
+    private String name;
+    private int lenght;
+    public Street(String name,int lenght) {
+        this.name=name;
+        this.lenght=lenght;
+    }
+}
